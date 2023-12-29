2db84b16e74921a49a8ae450f6b4a9e12acc7daa
Vasyl Chyypesh <vasyl.chyypesh@gmail.com>
2023-12-29 18:56:44 +0200

    ci: add task definition, update workflow

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -6,22 +6,24 @@ import { Logger } from '../utils/logger';
 const router = Router();
 
 router.use((req: Request, res: Response, next: NextFunction) => {
-  // eslint-disable-next-line no-console
   Logger.log(`request: ${req.method} ${req.originalUrl}`);
   next();
+});
+
+router.get('/health', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'ok' });
 });
 
 router.post('/input', input);
 
 router.get('/output/:ticket', output);
 
-router.use((req, res) => {
+router.use((req: Request, res: Response) => {
   res.status(404).json({ message: 'PAGE NOT FOUND' });
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 router.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
-  // eslint-disable-next-line no-console
   Logger.error(err);
 
   res.status(500).json({ message: 'INTERNAL SERVER ERROR' });
