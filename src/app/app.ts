06b84fabb763e5bb90febd00ee7b823fee06328d
vasyl-chyypesh <vasyl.chyypesh@gmail.com>
2024-02-26 19:05:43 +0200

    build: add helmet module, update dev dependencies

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import helmet from 'helmet';
 import router from './api/index.js';
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json({ strict: false, limit: '1mb' }));
 app.use(router);
 
