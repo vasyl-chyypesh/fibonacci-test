e2600ade0988589c5e50720955d5bfb585176071
vasyl-chyypesh <vasyl.chyypesh@gmail.com>
2025-08-09 20:37:36 +0300

    refactor: move performance test to test folder

--- test/fibonacci.perf.test.ts
+++ test/fibonacci.perf.test.ts
@@ -1,6 +1,6 @@
 import { describe, test } from 'node:test';
 import assert from 'node:assert';
-import { Fibonacci } from '../fibonacci.js';
+import { Fibonacci } from '../src/worker/fibonacci.js';
 
 describe('Fibonacci Performance', () => {
   describe('getValueFor', () => {
