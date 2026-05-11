7e2fa6f15796cfe682c4e2d3d1ed4b692369d89a
vasyl-chyypesh <vasyl.chyypesh@gmail.com>
2026-05-11 11:55:27 +0300

    feat: update rate and slow down limits

--- src/app/api/middlewares/rateLimiter.ts
+++ src/app/api/middlewares/rateLimiter.ts
@@ -3,7 +3,7 @@ import { CODES } from '../../utils/errors/codes.js';
 import { MESSAGES } from '../../utils/errors/messages.js';
 
 export const RATE_LIMIT_WINDOW = 1 * 60 * 1000; // 1 minute
-const RATE_LIMIT = 10; // limit each IP to 10 requests per `window`
+const RATE_LIMIT = 60; // limit each IP to 60 requests per `window`
 
 export const rateLimiter = rateLimit({
   windowMs: RATE_LIMIT_WINDOW,
--- src/app/api/middlewares/slowDownLimiter.ts
+++ src/app/api/middlewares/slowDownLimiter.ts
@@ -1,21 +1,20 @@
 import { slowDown } from 'express-slow-down';
 import { RATE_LIMIT_WINDOW } from './rateLimiter.js';
 
-const DELAY_AFTER = 5; // allow 5 requests per window without delay, it should be less then RATE_LIMIT
-const MAX_DELAY = 2000; // max delay in ms
+const DELAY_AFTER = 10; // allow 10 requests per window without delay, it should be less then RATE_LIMIT
+const MAX_DELAY = 1500; // max delay in ms
 
 export const slowDownLimiter = slowDown({
   windowMs: RATE_LIMIT_WINDOW,
   delayAfter: DELAY_AFTER,
-  delayMs: (hits) => hits * 100, // add 100 ms of delay to every request
+  delayMs: (hits) => hits * 10 + 500, // add 500 ms + 10 ms of delay to every request
   maxDelayMs: MAX_DELAY,
 
   /**
-   * - requests 1-5 are not delayed.
-   * - request 6 is delayed by 600ms
-   * - request 7 is delayed by 700ms
-   * - request 8 is delayed by 800ms
-   *
-   * and so on. After RATE_LIMIT_WINDOW duration, the delay is reset to 0.
+   * - requests 1-10 are not delayed.
+   * - request 11 is delayed by 610ms
+   * - request 12 is delayed by 620ms
+   * - request 13 is delayed by 630ms
+   * - and so on. After RATE_LIMIT_WINDOW duration, the delay is reset to 0.
    */
 });
